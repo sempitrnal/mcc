679dce10acbc61e7f6056d743757f1dd9f9aee0d
Reynald Sampelo <reynaldsampelo@bo-mbp-69.local>
2022-08-05 03:04:16 +0800

    percentage fix and legends for instructions

--- src/App.js
+++ src/App.js
@@ -6,9 +6,30 @@ function App() {
 			<div className="wrapper">
 				<p className="text-2">MacOS Calculator</p>
 				<small className="text-3">
-					You can use your <span className="font-[800]">Numpad</span>
-					or <span className="font-[800]">Number keys</span> for the
-					calculator :)
+					You can use your <span className="font-[800]">Numpad </span>
+					or{" "}
+					<span className="font-[800]">
+						Number and operator keys{" "}
+					</span>{" "}
+					for the calculator :)
+				</small>
+				<small className="text-4 flex flex-col">
+					<div className="">
+						<span className="font-extrabold">
+							A or C <span className="mx-1">=</span>{" "}
+						</span>
+						All Clear
+					</div>
+					<div className="flex items-center">
+						<span className="font-bold text-lg">⌫ </span>{" "}
+						<span className="font-extrabold mx-1"> = </span>
+						Delete
+					</div>
+					<div className="flex items-center">
+						<span className="font-extrabold ">N </span>{" "}
+						<span className="font-extrabold mx-1"> = </span>
+						Toggle +/-
+					</div>
 				</small>
 				<Calculator />
 				<p className="text">
